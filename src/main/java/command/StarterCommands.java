aa6a3acbcb9000aef49af295ade051c9b2eb310e
fittsqo <fittsqo@gmail.com>
2021-08-16 01:26:41 -0700

    welcome image generator implemented (not complete), simwelcome implemented

--- src/main/java/command/StarterCommands.java
+++ src/main/java/command/StarterCommands.java
@@ -1,7 +1,9 @@
 package command;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.internal.JDAImpl;
@@ -10,6 +12,8 @@ public class StarterCommands extends ListenerAdapter {
 
     public void onMessageReceived(MessageReceivedEvent event)
     {
+        JDA jda = event.getJDA();
+
         if (event.getAuthor().isBot()) return;
         Message message = event.getMessage();
         String content = message.getContentRaw();
@@ -18,7 +22,7 @@ public class StarterCommands extends ListenerAdapter {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("Pong!").queue();
         } else if (content.equals("!simwelcome")) {
-
+            ((JDAImpl)jda).handleEvent(new GuildMemberJoinEvent(event.getJDA(), jda.getResponseTotal(), event.getMember()));
         }
     }
 }
--- src/main/java/listeners/JoinListener.java
+++ src/main/java/listeners/JoinListener.java
@@ -6,24 +6,46 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.URL;
+import java.util.Map;
 
 public class JoinListener extends ListenerAdapter {
 
-    String imagePath = "src/main/resources/images/discord_blank.jpg";
+    String imagePath = "src/main/resources/images/discord_blank2.jpg";
 
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
 
         User user = event.getUser();
 
         try {
-            BufferedImage image = ImageIO.read(new File(imagePath));
-            Graphics2D img = image.createGraphics();
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
             ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/main/resources/fonts/coolvetica rg.ttf")));
-            drawCenteredString(img, user.getAsTag() + " wants to be comfy :D", image.getWidth(), 8 * image.getHeight() / 10, new Font("coolvetica rg", Font.PLAIN, 70));
-            drawCenteredString(img, "member #" + event.getGuild().getMemberCount(), image.getWidth(), 13 * image.getHeight() / 15, new Font("coolvetica rg", Font.PLAIN, 50));
+
+            BufferedImage image = ImageIO.read(new File(imagePath));
+            Graphics2D g2d = image.createGraphics();
+            g2d.setRenderingHint(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.setRenderingHint(
+                    RenderingHints.KEY_STROKE_CONTROL,
+                    RenderingHints.VALUE_STROKE_PURE);
+            g2d.setRenderingHint(
+                    RenderingHints.KEY_RENDERING,
+                    RenderingHints.VALUE_RENDER_QUALITY);
+
+            drawCenteredString(g2d, user.getAsTag() + " wants to be comfy :D",
+                    image.getWidth(), 8 * image.getHeight() / 10, new Font("coolvetica rg", Font.PLAIN, 80));
+            drawCenteredString(g2d, "member #" + event.getGuild().getMemberCount(),
+                    image.getWidth(), 13 * image.getHeight() / 15, new Font("coolvetica rg", Font.PLAIN, 50));
+
+            BufferedImage pfp = ImageIO.read(new URL(user.getAvatarUrl() + "?size=512"));
+            g2d.setStroke(new BasicStroke(20));
+            drawCenteredImage(g2d, pfp, image.getWidth(), (image.getHeight() - pfp.getHeight()) / 2 - 60);
+
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(image, "jpg", baos);
             byte[] bytes = baos.toByteArray();
@@ -34,10 +56,17 @@ public class JoinListener extends ListenerAdapter {
         }
     }
 
-    public void drawCenteredString(Graphics g, String text, int imageWidth, int textHeight, Font font) {
-        FontMetrics metrics = g.getFontMetrics(font);
-        g.setFont(font);
-        int x = (imageWidth - metrics.stringWidth(text)) / 2;
-        g.drawString(text, x, textHeight);
+    public void drawCenteredString(Graphics g2d, String text, int backgroundWidth, int textHeight, Font font) {
+        FontMetrics metrics = g2d.getFontMetrics(font);
+        g2d.setFont(font);
+        int x = (backgroundWidth - metrics.stringWidth(text)) / 2;
+        g2d.drawString(text, x, textHeight);
+    }
+
+    public void drawCenteredImage(Graphics2D g2d, BufferedImage img, int backgroundWidth, int foregroundHeight) {
+        int x = (backgroundWidth - img.getWidth()) / 2;
+        g2d.drawArc(backgroundWidth/2-256, foregroundHeight, 512, 512, 0, 360);
+        g2d.setClip(new Ellipse2D.Double((double)backgroundWidth/2-256, foregroundHeight, 512, 512));
+        g2d.drawImage(img, x, foregroundHeight, null);
     }
 }
