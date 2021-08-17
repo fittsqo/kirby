3d0c865ac2985976faf8e80596007b4e2e55eaa7
fittsqo <fittsqo@gmail.com>
2021-08-17 00:59:13 -0700

    added confirmation messages to commands (yes ik the commands being in 1 class is weird, this is temporary)

--- src/main/java/Commands/StarterCommands.java
+++ src/main/java/Commands/StarterCommands.java
@@ -25,16 +25,23 @@ public class StarterCommands extends ListenerAdapter {
             channel.sendMessage("Pong!").queue();
         }
         else if (content.equals("!simwelcome")) {
-            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.ADMINISTRATOR))
-                ((JDAImpl)jda).handleEvent(new GuildMemberJoinEvent(event.getJDA(), jda.getResponseTotal(), Objects.requireNonNull(event.getMember())));
+            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.ADMINISTRATOR)) {
+                ((JDAImpl) jda).handleEvent(new GuildMemberJoinEvent(event.getJDA(), jda.getResponseTotal(), Objects.requireNonNull(event.getMember())));
+                event.getChannel().sendMessage("simulated welcome message. if the message did not show up, try " +
+                        "`!setwelcomechannel` and then run the command again.").queue();
+            }
         }
         else if (content.equals("!setwelcomechannel")) {
-            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.ADMINISTRATOR))
+            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.ADMINISTRATOR)) {
                 MySQLInterfacer.setWelcomeChannel(event.getGuild().getId(), event.getChannel().getId());
+                event.getChannel().sendMessage("welcome channel set!").queue();
+            }
         }
         else if (content.contains("!setwelcomemessage")) {
-            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.ADMINISTRATOR))
+            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.ADMINISTRATOR)) {
                 MySQLInterfacer.setWelcomeMessage(event.getGuild().getId(), content.replace("!setwelcomemessage ", ""));
+                event.getChannel().sendMessage("welcome message set!").queue();
+            }
         }
     }
 }
