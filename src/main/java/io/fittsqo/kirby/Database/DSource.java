c32e168bbad7faafc501cced2c324d6d4ac364b8
fittsqo <fittsqo@gmail.com>
2021-09-01 17:19:12 -0400

    fixed stupid bug that caused connections to be made every query

--- src/main/java/io/fittsqo/kirby/Database/DSource.java
+++ src/main/java/io/fittsqo/kirby/Database/DSource.java
@@ -8,25 +8,23 @@ import java.sql.SQLException;
 
 public class DSource {
 
-    private final HikariConfig config = new HikariConfig();
-    private final String username;
-    private final String password;
+    private final HikariDataSource ds;
 
     public DSource(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public Connection getConnection() throws SQLException {
+        HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/kirbybase?useUnicode=yes&characterEncoding=UTF-8");
         config.setUsername(username);
         config.setPassword(password);
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        config.setMaximumPoolSize(3);
         config.setIdleTimeout(28800 - 60);
         config.setMaxLifetime(28800 - 60);
-        HikariDataSource ds = new HikariDataSource(config);
+        this.ds = new HikariDataSource(config);
+    }
+
+    public Connection getConnection() throws SQLException {
         return ds.getConnection();
     }
 }
